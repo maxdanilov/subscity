2af42bb98fd33837150d5e25b784c35b52754824
maxdanilov <admin@maxdanilov.ru>
2014-11-09 00:14:51 +0300

    imdb ratings now fetched from imdb, not kinopoisk + minor design fixes

--- app/helpers/kinopoisk.rb
+++ app/helpers/kinopoisk.rb
@@ -11,25 +11,57 @@ class Kinopoisk
 		end
 	end
 
-	def self.get_ratings(id)
-		data = Kinopoisk.fetch_rating_xml(id);
+	def self.get_imdb_rating(imdb_id)
+		url = "http://www.imdb.com/title/tt#{imdb_id.to_s.rjust(8, "0")}/"
+		error = false
+		begin
+			doc = Hpricot(open(url))
+			rating = doc.at("[@itemprop=ratingValue]").inner_text.to_f rescue nil
+			votes = doc.at("[@itemprop=ratingCount]").inner_text.gsub(/[^0-9]/, '').to_i rescue nil
+		rescue
+			rating, votes = nil
+			error = true;
+		end
+		{:error => error, :rating => rating, :votes => votes}
+	end
+
+	def self.get_kinopoisk_rating(kinopoisk_id)
+		data = Kinopoisk.fetch_rating_xml(kinopoisk_id);
 		error = false;
 		begin
 			doc = Hpricot(data)
-			kinopoisk_rating = doc.at("kp_rating").inner_text.to_f
-			kinopoisk_votes = doc.at("kp_rating")["num_vote"].to_i
-			imdb_rating = doc.at("imdb_rating").inner_text.to_f rescue nil
-			imdb_votes = doc.at("imdb_rating")["num_vote"].to_i rescue nil
+			rating = doc.at("kp_rating").inner_text.to_f
+			votes = doc.at("kp_rating")["num_vote"].to_i
+			#imdb_rating = doc.at("imdb_rating").inner_text.to_f rescue nil
+			#imdb_votes = doc.at("imdb_rating")["num_vote"].to_i rescue nil
 		rescue
-			kinopoisk_rating, kinopoisk_votes, imdb_rating, imdb_votes = nil
+			rating, votes = nil
 			error = true;
 		end
+		{:error => error, :rating => rating, :votes => votes}
+	end
+
+	def self.get_ratings(kinopoisk_id, imdb_id)
+		kp = get_kinopoisk_rating(kinopoisk_id) rescue nil
+		if imdb_id.to_i == 0
+			imdb = { :error => false }
+		else
+			imdb = get_imdb_rating(imdb_id) rescue nil
+		end
+		
+		error = (kp[:error] or imdb[:error]) rescue true
+		kinopoisk_rating = kp[:rating] rescue nil
+		kinopoisk_votes = kp[:votes] rescue nil
+		imdb_rating = imdb[:rating] rescue nil
+		imdb_votes = imdb[:votes] rescue nil
+
 		{:error => error, :kinopoisk => {:rating => kinopoisk_rating, :votes => kinopoisk_votes}, :imdb => {:rating => imdb_rating, :votes => imdb_votes}}
 	end
 
 	def self.update_ratings(c)
 		puts "\tUpdating rating #{c.kinopoisk_id}..."#.yellow
-		result = Kinopoisk.get_ratings(c.kinopoisk_id) rescue nil
+		result = Kinopoisk.get_ratings(c.kinopoisk_id, c.imdb_id) rescue nil
+		
 		unless result.nil? or result[:error] == true
 			r = nil
 			if Rating.exists?(:movie_id => c.movie_id)
@@ -44,7 +76,6 @@ class Kinopoisk
 			r[:kinopoisk_votes] = result[:kinopoisk][:votes].to_i
 			r[:imdb_rating] = result[:imdb][:rating].to_f
 			r[:imdb_votes] = result[:imdb][:votes].to_i
-
 			r.save
 			puts "\t\t Update result: #{result}..."
 		else
--- restart.sh
+++ restart.sh
@@ -1,3 +1,10 @@
-﻿#!/bin/bash
+#!/bin/bash
+MODE=${1:-production}    
+if [[ $MODE =~ ^d ]]; then
+   MODE="development"
+else
+   MODE="production"
+fi
+
 padrino stop
-padrino start -d -h 0.0.0.0 -a thin -e production
+padrino start -d -h 0.0.0.0 -a thin -e $MODE
