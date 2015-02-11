d4a6da119129e0be26afc6e95f59a9870ae01fe5
maxdanilov <admin@maxdanilov.ru>
2015-02-12 01:52:28 +0300

    minor design fixes

--- app/settings.rb
+++ app/settings.rb
@@ -2,7 +2,7 @@ DOMAIN_NAME = 'subscity.ru'
 
 SETTINGS =  {
             	:screenings_show_span => 8,    				# in days
-            	:screenings_fetch_span => 8,   				# in days
+            	:screenings_fetch_span => 5,   				# in days
             	:movie_new_span => 9,						# in days
             	:sell_tickets => false,          			# sell tix within site or redirect to kassa
             	                                			# if sold within, 10% comission applied
