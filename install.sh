0f43773eb136418f35d45fbbecee893df5775cd4
Maxim Danilov <maxim.danilov@smartb.eu>
2016-07-09 23:01:59 +0300

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ RUBY_VERSION="2.3.0"
 INSTALL_DIR="/root/subscity/"
 INSTALL_DIR_ROOT="/var/www/"
 INSTALL_DIR="${INSTALL_DIR_ROOT}subscity/"
-usermod -a -G www-data root
+
+sudo usermod -a -G www-data root
 #installing extra packages
 
 sudo apt-get update
@@ -19,7 +20,7 @@ sudo dpkg-reconfigure tzdata
 sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -L https://get.rvm.io | bash -s stable
-source /usr/local/rvm/scripts/rvm
+source ~/.rvm/scripts/rvm
 rvm install $RUBY_VERSION
 rvm use $RUBY_VERSION --default
 
@@ -27,20 +28,23 @@ sudo apt-get install realpath unzip git mysql-server libmysqlclient-dev
 sudo mysql_secure_installation
 
 sudo apt-get install phpmyadmin
-php5enmod mcrypt
+sudo php5enmod mcrypt
 # change phpmyadmin URL
 sed -i 's/Alias \/phpmyadmin/Alias \/pad4/g' /etc/phpmyadmin/apache.conf
-echo "" > /var/www/html/index.html
+sudo echo "" > /var/www/html/index.html
 # enabling apache modules
-a2enmod proxy proxy_http headers deflate mime expires
+a2enmod proxy proxy_http headers deflate mime expires ssl rewrite
+
+cd ${INSTALL_DIR_ROOT}
+git clone https://github.com/maxdanilov/subscity.git
+
 # copying apache config and enabling site
-cp ${INSTALL_DIR}/config/apache.site.conf /etc/apache2/sites-available/subscity.conf
-a2ensite subscity
+cp ${INSTALL_DIR}/config/subscity.conf /etc/apache2/sites-available/
+cp ${INSTALL_DIR}/config/subscity-ssl.conf /etc/apache2/sites-available/
+a2ensite subscity subscity-ssl
 service apache2 restart
 
 wget http://mysqltuner.pl/ -O ${INSTALL_DIR_ROOT}mysqltuner.pl
-
-# example of apache config
 
 # in pad create a 'subscity' db
 # import mysql backup into 'subscity' db
@@ -48,20 +52,18 @@ wget http://mysqltuner.pl/ -O ${INSTALL_DIR_ROOT}mysqltuner.pl
 # gems and dependencies
 sudo apt-get install mailutils mutt
 sudo apt-get install libgmp3-dev
-sudo apt-get install libmagickwand-dev imagemagick libmagickcore-dev graphicsmagick-libmagick-dev-compat
-
-#gem install eventmachine sqlite3 rmagick
-gem install whenever
+sudo apt-get install graphicsmagick-libmagick-dev-compat
+sudo apt-get install libmagickcore-dev
+sudo apt-get install libmagickwand-dev imagemagick
 
 sudo apt-get install bundler
+gem install whenever
 gem install bundle
 
 # subscity itself
-cd ${INSTALL_DIR_ROOT}
-git clone https://github.com/maxdanilov/subscity.git
 cd ${INSTALL_DIR}
-config.sh
 bundle install
+config.sh
 
 # setting cron to run & check padrino
 
