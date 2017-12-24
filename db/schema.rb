73f325d2f9ba086e08e82cd451311b57c1c39a2b
Maxim Danilov <maxim.danilov@smartb.eu>
2017-12-24 17:31:41 +0100

    Updated db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -29,19 +29,21 @@ ActiveRecord::Schema.define(:version => 7) do
     t.string   "metro"
     t.string   "url"
     t.string   "phone"
-    t.boolean  "fetch_all"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.decimal  "longitude",
-    t.decomal  "latitude"
+    t.boolean  "fetch_all",                                  :default => false, :null => false
+    t.datetime "created_at",                                                    :null => false
+    t.datetime "updated_at",                                                    :null => false
+    t.decimal  "longitude",  :precision => 15, :scale => 10
+    t.decimal  "latitude",   :precision => 15, :scale => 10
   end
+
+  add_index "cinemas", ["cinema_id", "city_id"], :name => "cinema_id"
 
   create_table "cities", :force => true do |t|
     t.boolean  "active"
     t.string   "domain"
     t.integer  "city_id"
     t.string   "name"
-    t.string   "name_short"
+    t.string   "name_short", :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -64,12 +66,24 @@ ActiveRecord::Schema.define(:version => 7) do
     t.integer  "duration"
     t.integer  "kinopoisk_id"
     t.integer  "imdb_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                                      :null => false
+    t.datetime "updated_at",                                      :null => false
     t.string   "trailer"
-    t.integer  "fetch_mode"
+    t.integer  "fetch_mode",          :limit => 1, :default => 0, :null => false
     t.boolean  "hide"
   end
+
+  create_table "ratings", :force => true do |t|
+    t.datetime "updated_at"
+    t.datetime "created_at"
+    t.integer  "movie_id"
+    t.float    "kinopoisk_rating"
+    t.integer  "kinopoisk_votes"
+    t.float    "imdb_rating"
+    t.integer  "imdb_votes"
+  end
+
+  add_index "ratings", ["id"], :name => "id", :unique => true
 
   create_table "screenings", :force => true do |t|
     t.integer  "movie_id"
@@ -82,13 +96,6 @@ ActiveRecord::Schema.define(:version => 7) do
     t.datetime "updated_at",   :null => false
   end
 
-  create_table "ratings", :force => true do |t|
-    t.integer  "movie_id"
-    t.real     "kinopoisk_rating"
-    t.integer  "kinopoisk_votes"
-    t.real     "imdb_rating"
-    t.integer  "imdb_votes"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-  end
+  add_index "screenings", ["cinema_id"], :name => "cinema_id"
+
 end
