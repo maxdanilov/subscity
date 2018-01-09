39d2ac927b15ef82d9c0fc594688d86918d8f877
Maxim Danilov <maxim.danilov@smartb.eu>
2018-01-09 22:27:15 +0100

    Fix pull.sh

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -3,5 +3,5 @@ set -e
 
 cd "${0%/*}/../"
 docker pull maximdanilov/subscity:$(git rev-parse --abbrev-ref HEAD)-latest
-docker-pull nginx:latest
-docker-pull mysql:5.7.20
+docker pull nginx:latest
+docker pull mysql:5.7.20
