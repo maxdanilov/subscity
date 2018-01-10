1fba96129d776bca42ab706b78e50102321f96fb
Maxim Danilov <maxim.danilov@smartb.eu>
2018-01-10 22:40:21 +0100

    Fix renew certs

--- scripts/renewcerts.sh
+++ scripts/renewcerts.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
+# initial cert generation:
+# letsencrypt certonly --webroot -w /var/www/subscity/public/ \
+# -d subscity.ru -d www.subscity.ru -d msk.subscity.ru -d spb.subscity.ru
+
 cd "${0%/*}/../"
-./letsencrypt-auto renew
+letsencrypt renew
 cp -L /etc/letsencrypt/live/subscity.ru/*.pem dockerfiles/certs/production/
 docker-compose restart nginx
