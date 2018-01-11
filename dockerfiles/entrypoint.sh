afad764a39e48dcb9e398547bbb43c8ea4739c5b
Maxim Danilov <maxim.danilov@smartb.eu>
2018-01-11 22:51:42 +0100

    Fix fill_cache task

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "127.0.0.1 local.host msk.local.host spb.local.host" >> /etc/hosts
 echo "GEM_PATH=/usr/local/bundle/" | crontab -
 (crontab -l ; echo "$(printenv | grep ^SC_)") | crontab -
 cd tasks && whenever --update-crontab && cd ..
