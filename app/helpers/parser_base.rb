8e6f3e83675b28a762a077a31d14721b92a5ee60
Maxim Danilov <maxim.danilov@smartb.eu>
2018-01-17 22:39:34 +0100

    Rubocop logger.rb and parser_base.rb

--- app/helpers/parser_base.rb
+++ app/helpers/parser_base.rb
@@ -1,26 +1,21 @@
 require 'json'
 
 module ParserBase
-	def get_first_regex_match(str, regex)
-		nil unless str.is_a? String
-		result = str.match(regex)
-		if result.nil?
-			nil
-		else
-			result[1]
-		end
-	end
+  def get_first_regex_match(str, regex)
+    nil unless str.is_a? String
+    result = str.match(regex)
+    if result.nil?
+      nil
+    else
+      result[1]
+    end
+  end
 
-	def get_first_regex_match_integer(str, regex)
-		get_first_regex_match(str, regex).to_i
-	end
+  def get_first_regex_match_integer(str, regex)
+    get_first_regex_match(str, regex).to_i
+  end
 
-	def parse_json(data)
-		begin
-			return JSON.parse(data)
-		rescue => e
-			{}
-		end
-	end
-
+  def parse_json(data)
+    JSON.parse(data) rescue {}
+  end
 end
