63b3796d3987ba702b67db3312a127cec1d8a3e1
Maxim Danilov <maxim.danilov@smartb.eu>
2018-01-17 22:35:20 +0100

    Rubocop cache.rb

--- app/helpers/cache.rb
+++ app/helpers/cache.rb
@@ -1,10 +1,9 @@
 class FileCache
-	def self.expire
-		FileUtils.rm_rf(Dir.glob(self.dir + '/*'))
-	end
+  def self.expire
+    FileUtils.rm_rf(Dir.glob(dir + '/*'))
+  end
 
-	def self.dir
-		#Padrino.root('tmp', app_name.to_s, 'cache')
-		File.expand_path(File.dirname(__FILE__)) + "/../../tmp/subscity/app/cache"
-	end
+  def self.dir
+    File.expand_path(__dir__) + '/../../tmp/subscity/app/cache'
+  end
 end
